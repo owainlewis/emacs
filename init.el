5ea87d86edbc73ec26bf959d45becf2ee4759ef9
Owain Lewis <owain@owainlewis.com>
2018-05-08 08:44:13 +0100

    Update init.el

--- init.el
+++ init.el
@@ -10,6 +10,38 @@
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+(when (window-system)
+  (set-frame-font "Fira Code 14"))
+(let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
+	       (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
+	       (36 . ".\\(?:>\\)")
+	       (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
+	       (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+	       (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
+	       (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
+	       (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+	       (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
+	       (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+	       (48 . ".\\(?:x[a-zA-Z]\\)")
+	       (58 . ".\\(?:::\\|[:=]\\)")
+	       (59 . ".\\(?:;;\\|;\\)")
+	       (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
+	       (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+	       (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+	       (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
+	       (91 . ".\\(?:]\\)")
+	       (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+	       (94 . ".\\(?:=\\)")
+	       (119 . ".\\(?:ww\\)")
+	       (123 . ".\\(?:-\\)")
+	       (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+	       (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
+	       )
+	     ))
+  (dolist (char-regexp alist)
+    (set-char-table-range composition-function-table (car char-regexp)
+						    `([,(cdr char-regexp) 0 font-shape-gstring]))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (load "package")
@@ -29,9 +61,11 @@
   go-autocomplete
   flymake-go
   haskell-mode
+  intero
+  neotree
   magit
   markdown-mode
-  monokai-theme
+  atom-one-dark-theme
   yaml-mode)
   "Default packages")
 
@@ -48,26 +82,20 @@
     (when (not (package-installed-p pkg))
       (package-install pkg))))
 
-;; UI
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(load-theme 'monokai t)
 
-;; Haskell mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(custom-set-variables
- '(haskell-stylish-on-save t))
-
-;; Generic hooks and setup
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(load-theme 'atom-one-dark t)
 
 (add-hook 'before-save-hook 'whitespace-cleanup)
+(add-hook 'before-save-hook #'gofmt-before-save)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (ac-config-default)
 (require 'auto-complete-config)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'go-autocomplete)
